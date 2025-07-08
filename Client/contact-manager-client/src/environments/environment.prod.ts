10e53d610b70b174f216d0a66b533857d6a85569
mahmoudadel810 <ma.adel.810@gmail.com>
2025-07-08 19:36:21 +0300

    both enviroment for developing or production , both handled to get easly deployment process

---
+++ Client/contact-manager-client/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:8000/api/v1', // Update this for production
+  tokenPrefix: 'Contact__' // Matches backend TOKEN_PREFIX
+}; 
---
+++ Client/contact-manager-client/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8000/api/v1',
+  tokenPrefix: 'Contact__' // Matches backend TOKEN_PREFIX
+}; 
